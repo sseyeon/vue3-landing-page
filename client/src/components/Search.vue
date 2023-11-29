b7a80dd7c8c0a3feab2061bbabbf9ffa74d7f494
sseyeon <seyeon9809@gmail.com>
2023-11-29 17:59:07 +0900

    [F/add] 뉴스 페이지 생성

---
+++ client/src/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- 검색 -->
+  <section class="container">
+    <div
+      class="sm:w-3/4 lg:w-5/12 mt-4 relative rounded-3xl shadow-sm text-center mx-auto"
+    >
+      <input
+        type="text"
+        class="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:border-blue-300 focus:ring focus:ring-blue-200 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+        placeholder="검색어를 입력하세요"
+      />
+      <div
+        class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+      >
+        <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ client/src/components/TitleComponent.vue
@@ -0,0 +1,26 @@
+<!-- TitleComponent.vue -->
+<template>
+  <section class="mt-10">
+    <!-- Heading -->
+    <div class="container text-center">
+      <h1 class="text-blue-700 font-semibold text-xl">{{ titleText }}</h1>
+      <h1 class="mt-4 font-semibold text-3xl" v-html="subText"></h1>
+    </div>
+  </section>
+</template>
+  
+  <script>
+export default {
+  props: {
+    titleText: {
+      type: String,
+      required: true,
+    },
+    subText: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+  
